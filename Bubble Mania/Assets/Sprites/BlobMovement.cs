c91ba83ad366a90a72f9790bffb64010f371fcc6
bertlomewos <160644577+bertlomewos@users.noreply.github.com>
2025-01-24 22:31:17 +0300

    first commit

---
+++ Bubble Mania/Assets/Sprites/BlobMovement.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BlobMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+
+    [SerializeField] private float speed = 8f;
+    [SerializeField] private float jumpingPower = 12f;
+    private float horizontal;
+    private bool isFacingRight = true;
+
+
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+
+        if(!isFacingRight && horizontal > 0f)
+        {
+            flip();
+        }
+        else if(isFacingRight && horizontal < 0f)
+        {
+            flip();
+        }
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed && isGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+
+        if(context.canceled && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
+    }
+
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+}
